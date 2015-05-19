115aeb8055a6597b528b17e2784e97cea817191b
Oliver Warne <oliverswarne@gmail.com>
2015-05-19 22:05:49 +0000

    Added more bot stuff. General bug fixing

--- furtumTest.py
+++ furtumTest.py
@@ -1,11 +1,12 @@
 import praw
 import kdapi
 import time
+import linecache
 from operator import itemgetter
 
-r = praw.Reddit(user_agent='Test Script by u/{}'.format('INSERT USERNAME'))
+r = praw.Reddit(user_agent='Test Script by u/{}'.format(INSERT USERNAME))
 
-r.login('BOTUSERNAME','PASSWORD')
+r.login('USERNAME','PASSWORD')
 
 ################################################################################################################################################
 #                                                         Top comment grabber                                                                  #
@@ -15,7 +16,7 @@ r.login('BOTUSERNAME','PASSWORD')
 def karmaCommentOBJ(submission,link):
     karmalist = []
     for item in kdapi.check(link):
-        if item.score != None and item.score != "None" and item.score >= 60 and submission.short_link.split('/')[3] != item.link.split("/")[6]:
+        if item.score != None and item.score != "None" and submission.short_link.split('/')[3] != item.link.split("/")[6]:
 
             # ^^^ Check for if a) has score b) is greater than 500 and c) is not the same thing that we are looking for
             karmaID = item.link.split("/")[6]  # Karma{} is just the stuff i have stolen.
@@ -37,8 +38,6 @@ def karmaCommentLink(karmaLink):
     else:
         return sorted(karmalist,key=itemgetter(1))[-1][0]
 
-
-
 ################################################################################################################################################
 #                                                               Botty Stuff                                                                    #
 ################################################################################################################################################
@@ -46,16 +45,42 @@ def karmaCommentLink(karmaLink):
 already_done = []
 while True:
     subreddit = r.get_subreddit('all')
-    for submission in subreddit.get_rising(limit=10):
+    for submission in subreddit.get_new(limit=20):
         print submission
-        print str(submission.permalink)
-        topComment = karmaCommentLink(str(submission.permalink))
+        try:
+            topComment = karmaCommentLink(str(submission.permalink))
+        except UnicodeEncodeError:
+            continue
         if topComment != None and topComment not in already_done:
             print "worked: " + topComment
-            already_done.append([topComment,str(submission.permalink)])
+            
             with open("testfile.txt","a") as f:
-                f.write(str(already_done))
-            #submission.reply(topComment)
+                try:
+                    f.write(str([topComment,submission.permalink]))
+                    f.write("\n")
+                    submission.add_comment(str(topComment))
+                except UnicodeEncodeError:
+                    f.write("['String broke :(((']")
+                    f.write(',')
+                    f.write("\n")
+                    continue
+                except praw.errors.RateLimitExceeded:
+                    print "commenting too fast!"
+                    print "going to sleep!"
+                    time.sleep(150)
+                    print "halfway through!"
+                    time.sleep(150)
+                    print "done sleeping"
+                    try:
+                        submission.add_comment(str(topComment))
+                    except UnicodeEncodeError:
+                        f.write("['String broke :(((']")
+                        f.write(',')
+                        f.write("\n")
+                        continue
         else:
             print "failed :("
-    time.sleep(2000)
+    print "sleeping!"
+    time.sleep(120)
+
+
