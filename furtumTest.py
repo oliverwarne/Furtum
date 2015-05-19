4c6c85779107be51879113615a44bcc82b0e2182
Oliver Warne <oliverswarne@gmail.com>
2015-05-19 14:16:57 +0000

    Botified. Bot is broken though

--- furtumTest.py
+++ furtumTest.py
@@ -1,43 +1,31 @@
 import praw
 import kdapi
+import time
 from operator import itemgetter
 
 r = praw.Reddit(user_agent='Test Script by u/{}'.format('INSERT USERNAME'))
 
 r.login('BOTUSERNAME','PASSWORD')
 
+################################################################################################################################################
+#                                                         Top comment grabber                                                                  #
+################################################################################################################################################
+
+
 def karmaCommentOBJ(submission,link):
     karmalist = []
     for item in kdapi.check(link):
-        finalComment = "broken :((((("
-        # print item.title
-        # print str(item.score) + " score" 
-        if item.score != None and item.score != "None" and item.score >= 500 and submission.short_link.split('/')[3] != item.link.split("/")[6]:
+        if item.score != None and item.score != "None" and item.score >= 60 and submission.short_link.split('/')[3] != item.link.split("/")[6]:
+
             # ^^^ Check for if a) has score b) is greater than 500 and c) is not the same thing that we are looking for
             karmaID = item.link.split("/")[6]  # Karma{} is just the stuff i have stolen.
-            karmasubmission = r.get_submission(submission_id = karmaID,comment_limit = 0)  # Submis OBJ for KD
-
+            karmasubmission = r.get_submission(submission_id = karmaID,comment_limit = 10)  # Submis OBJ for KD
             karmasubmission.replace_more_comments(limit=0, threshold=0)  # Messing with morecomments is dumb, this works
-
-
             karmalist = []
             for karmacomment in karmasubmission.comments:  # iterates through top 10 comments and grabs top
                 if karmacomment.body != "None" and "(/" not in karmacomment.body and karmacomment.body != "[deleted]" and karmacomment.is_root:
                     tuplelist = [karmacomment.body,karmacomment.score]
                     karmalist.append(tuplelist)
-                    finalComment = "Did pass " + str(karmacomment.body)
-                else:
-                    finalComment = "None - Failed all"
-        ### DEBUG STATEMENTS - REMOVE FROM RELEASE ###
-        else:
-            if item.score == "None":
-                print "itmescore is none"
-            if item.score <= 500:
-                print "itemscore too low"
-            if submission.short_link.split('/')[3] == item.link.split("/")[6]:
-                print "same thing"
-            print "did not pass"
-        ### DEBUG STATEMENTS - REMOVE FROM RELEASE ###
     return karmalist
 
 def karmaCommentLink(karmaLink):
@@ -48,7 +36,26 @@ def karmaCommentLink(karmaLink):
         return None
     else:
         return sorted(karmalist,key=itemgetter(1))[-1][0]
-        
 
 
-print karmaCommentLink("http://www.reddit.com/r/pics/comments/2z0lm7/miss_america_1924/")
+
+################################################################################################################################################
+#                                                               Botty Stuff                                                                    #
+################################################################################################################################################
+
+already_done = []
+while True:
+    subreddit = r.get_subreddit('all')
+    for submission in subreddit.get_rising(limit=10):
+        print submission
+        print str(submission.permalink)
+        topComment = karmaCommentLink(str(submission.permalink))
+        if topComment != None and topComment not in already_done:
+            print "worked: " + topComment
+            already_done.append([topComment,str(submission.permalink)])
+            with open("testfile.txt","a") as f:
+                f.write(str(already_done))
+            #submission.reply(topComment)
+        else:
+            print "failed :("
+    time.sleep(2000)
