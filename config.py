18ff0686f1860db1fb53d6114c75fbe4adb19846
OliverWarne <oliverswarne@gmail.com>
2015-05-23 17:31:04 -0500

    added external config

---
+++ config.py
@@ -0,0 +1,6 @@
+creatorName = ""  # Your reddit username
+
+botName = ""  # Bots username
+botPass = ""  # Bot password
+
+scanSub = ["pics", "funny", "TodayILearned", "aww"]  # To add your own subs to scan, just add them seperated via comma
